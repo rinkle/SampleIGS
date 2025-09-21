444e0f4af91e7393b7dddbbeacffd3a2110f3b95
Rinkle Cleverdesign <rinkle.del@gmail.com>
2025-09-21 18:46:05 +0530

    igs

--- IGS2025/IGS.Web/obj/Debug/net9.0/IGS.Web.AssemblyInfo.cs
+++ IGS2025/IGS.Web/obj/Debug/net9.0/IGS.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IGS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114db7f8d368d3c58c9d03fb8913ab9cb87dfec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ebe868b0131146a9eee9de5e8351d59c0e20ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("IGS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IGS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
